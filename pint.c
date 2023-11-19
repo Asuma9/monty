ff67c6887206c198526d9eef8d1cb9ede8387688
Asuma9 <paulasuma10@gmail.com>
2023-11-19 10:41:22 +0300

    Prints the value at the top of the stack

--- pint.c
+++ pint.c
@@ -7,7 +7,7 @@
  */
 void pint(stack_t **head, unsigned int line_num)
 {
-	if (*head == NULL)
+	if (!head || !(*head))
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
